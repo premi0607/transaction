82fe932a57124e6529f8c51c7a4b55712aaddab4
premiikha_s <spremikhha@gmail.com>
2023-06-17 09:16:00 -0700

    create-expense-ui

--- src/app/transervice.service.ts
+++ src/app/transervice.service.ts
@@ -8,8 +8,8 @@ export class TranserviceService {
 
   constructor(private http: HttpClient) { }
 
-  updatateExpense(id:any, expense:any){
-    return this.http.put('https://648a952117f1536d65e94eb1.mockapi.io/expense'+'/'+id, expense)
+  updatateExpense(id:any, newExpense:any){
+    return this.http.put('https://648a952117f1536d65e94eb1.mockapi.io/expense'+'/'+id, newExpense)
   }
 
   createExpense(newexpense:any){
